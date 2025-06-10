045a1d400dbd83d4084e6e635a11ac6745788eb9
Jun Myeong <hab0803@cau.ac.kr>
2025-06-10 11:44:48 +0900

    feat: add prize search and extraction feature for news agencies

--- db.py
+++ db.py
@@ -73,6 +73,110 @@ class Database:
 
         return achievements
 
+
+    # 신문사별 당선작 링크 가져오기
+
+    class GetPrize:
+        def __init__(self):
+            self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"}
+
+        def donga(self):
+            prizes = []
+            for year in range(1998, 2026):
+                url = f"https://sinchoon.donga.com/List?m=year&c={year}"
+                print(url)
+                req = requests.get(url, headers=self.headers)
+                soup = BeautifulSoup(req.content, 'html.parser')
+                prize_wrap = soup.find('div', class_='prize_wrap')
+                uls = prize_wrap.find_all('ul')
+                for ul in uls:
+                    lis = ul.find_all('li')
+                    for li in lis:
+                        ctgry = li.find(class_='category')
+                        title = li.find(class_='title')
+                        writer = li.find(class_='name')
+                        url = title.find('a').attrs['href']
+
+                        if "당선작 없음" in title.get_text() or "당선 취소" in title.get_text():
+                            continue
+
+                        data = {"category" : ctgry.get_text() if ctgry != None else "카테고리 불명",
+                             "url" : url if url != None else "링크 불명",
+                             "title" : title.get_text() if title != None else "제목 불명",
+                             "writer" : writer.get_text() if writer != None else "작자 미상",
+                             "press" : "동아일보"
+                            }
+
+                        res = requests.get(url, headers=self.headers)
+                        bs = BeautifulSoup(res.content, 'html.parser')
+                        contents = bs.find_all('ul', class_='tab_content')
+
+                        for li2, kind in zip(contents, ["content", "review", "thoughts"]):
+                            context = li2.find(class_="view_box")
+                            for br in context.find_all('br'):
+                                br.replace_with('\n')
+                            data[kind] = context.get_text()
+
+                        prizes.append(data)
+            return prizes
+
+        def chosun(self): # Chonsun Ilbo website is dynamically rendered, requiring Selenium for scraping
+            first = True
+
+            for year in range(2015, 2026):
+
+
+            for year in range(1998, 2026):
+                if first:
+                    url = f"https://www.chosun.com/nsearch/?query=%5B{year}%20%EC%8B%A0%EC%B6%98%EB%AC%B8%EC%98%88%5D&page=1"
+                    req = requests.get(url, headers=self.headers)
+                    soup = BeautifulSoup(req.content, 'html.parser')
+                    ul = soup.find('ul', class_='pageNumbers')
+                    lis = ul.find_all('li')
+                    al = len(lis) + 1
+                    first = False
+
+                if not 'al' in locals():
+                    al = 2
+
+                for page in range(1, al):
+                    url = f"https://www.chosun.com/nsearch/?query=%5B{year}%20%EC%8B%A0%EC%B6%98%EB%AC%B8%EC%98%88%5D&page={page}"
+                    req = requests.get(url, headers=self.headers)
+                    soup = BeautifulSoup(req.content, 'html.parser')
+                    article_list = soup.find('div', class_='story-card-wrapper')
+                    for article in article_list:
+                        tmp = article.find_all('div', recursive=False)
+                        a_ele = tmp[:1]
+                        a_url = a_ele.find('a')['href']
+
+                        req1 = requests.get(a_url, headers=self.headers)
+                        tree = html.fromstring(req1.content)
+                        soup1 = BeautifulSoup(req1.content, 'html.parser')
+                        title = soup1.find('h1', class_='article-header__headline')
+                        cate = tree.xpath('//*[@id="fusion-app"]/div[1]/div[2]/div/div/div[2]/p/span/text()')
+                        cate = cate.replace('당선작', '').strip('')
+                        writer = tree.xpath('//*[@id="fusion-app"]/div[1]/div[2]/div/section/article/div[1]/div/span/text()')
+
+                        ##
+
+
+
+
+
+
+
+    class FireBase:
+        def __init__(self):
+            cred = credentials.Certificate("key.json")
+            firebase_admin.initialize_app(cred)
+            self.db = firestore.client()
+
+        def add_from_list(self, data):
+            for datum in data:
+                if "/" in datum["title"]:
+                    datum["title"] = datum["title"].replace("/", "\\")
+                self.db.collection('prizes').document(datum["title"]).set(datum)
+
     def add_content(self, chunk):
         for key in self.article:
             if key in chunk['url']:
